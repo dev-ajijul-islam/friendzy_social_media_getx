caa9420be29c34f8e78040506845a1dde2184340
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-22 17:19:27 +0600

    post_model.dart created

---
+++ lib/data/models/post_model.dart
@@ -0,0 +1,45 @@
+import 'package:friendzy_social_media_getx/data/models/user_model.dart';
+
+class PostModel {
+  UserModel author;
+  String caption;
+  List<String> images;
+  List<String>? hashTags;
+  List<String>? likerIds;
+  List<String>? commenterIds;
+  DateTime createdAt;
+
+  PostModel({
+    required this.author,
+    required this.caption,
+    required this.images,
+    this.likerIds,
+    this.commenterIds,
+    this.hashTags,
+    required this.createdAt,
+  });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      author: json["author"],
+      caption: json["caption"],
+      commenterIds: json["commenterIds"],
+      hashTags: json["hashTags"],
+      likerIds: json["likerIds"],
+      images: json["images"],
+      createdAt: json["createdAt"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "author": author,
+      "caption": caption,
+      "commenterIds": commenterIds,
+      "hashTags": hashTags,
+      "likerIds": likerIds,
+      "images": images,
+      "createdAt": createdAt,
+    };
+  }
+}
